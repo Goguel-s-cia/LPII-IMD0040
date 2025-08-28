b02f3e58f0e0faa1c5ede9df70ddc61084c10f92
Miguel <miguelxmorais@gmail.com>
2025-08-28 20:02:17 -0300

    Atividade_04 | 28/08/2025

---
+++ Aluno.java
@@ -0,0 +1,81 @@
+package primeiraUnd;
+
+public class Aluno {
+	
+	private String nome;
+	private String matricula;
+	private Double media, nota1, nota2;
+	public static int totalAlunos = 0;
+	
+	/*
+	 * Construtor vazio
+	 */
+	public Aluno() {
+		this.totalAlunos++;
+	}
+	
+	/*
+	 * Construtor com argumentos
+	 */
+	public Aluno(String nome, String matricula) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.nota1 = 0.0;
+		this.nota2 = 0.0;
+		this.totalAlunos++;
+	}
+	
+	/*
+	 * Define a primeira nota do aluno
+	 */
+	public void setNota1(Double nota) {
+		if(nota >= 0.0 && nota <= 10.0) {
+			this.nota1 = nota;
+			return;
+		} 
+		this.nota1 = 0.0;
+	}
+
+	/*
+	 * Define a segunda nota do aluno
+	 */
+	public void setNota2(Double nota) {
+		if(nota >= 0.0 && nota <= 10.0) {
+			this.nota2 = nota;
+			return;
+		} 
+		this.nota2 = 0.0;
+	}
+	
+	/*
+	 * Calcula a média do aluno
+	 */
+	public void calcularMedia() {
+		this.media = (this.nota1 + this.nota2)/2;
+	}
+	
+	/*
+	 * Exibe as informações do aluno
+	 */
+	public void exibirInfo() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("Matrícula: " + this.matricula);
+		System.out.println("Nota 1: " + this.nota1);
+		System.out.println("Nota 2: " + this.nota2);
+		System.out.println("Média: " + this.media + "\n");
+	}
+	
+	/*
+	 * Verifica se o aluno está aprovado
+	 */
+	public void isAprovado() {
+		boolean aprovado = this.media >= 6.0 ? true : false;
+		
+		if(aprovado) {
+			System.out.println("O aluno " + this.nome + " está aprovado\n");
+			return;
+		}
+		
+		System.out.println("O aluno " + this.nome + " está reprovado\n");
+	}
+}
---
+++ Main.java
@@ -0,0 +1,38 @@
+package primeiraUnd;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Cria os alunos
+		Aluno a1 = new Aluno();
+		Aluno a2 = new Aluno("Roberval", "1234");
+		Aluno a3 = new Aluno("Marcos", "4321");
+		
+		//Define as notas
+		a1.setNota1(8.5);
+		a1.setNota2(3.0);
+		a2.setNota1(4.0);
+		a2.setNota2(6.0);
+		a3.setNota1(9.0);
+		a3.setNota2(10.0);
+		
+		// Calcula as médias
+		a1.calcularMedia();
+		a2.calcularMedia();
+		a3.calcularMedia();
+		
+		// Exibe as informações
+		a1.exibirInfo();
+		a2.exibirInfo();
+		a3.exibirInfo();
+		
+		// Exibe se o aluno está aprovado ou reprovado
+		a1.isAprovado();
+		a2.isAprovado();
+		a3.isAprovado();
+		
+		// Exibe o total de alunos
+		System.out.println("O total de alunos criados é de: " + Aluno.totalAlunos + " alunos");
+	}
+}
